aa17235eda7e4da40ce90eaa483914a6a38dcf0a
Wavilo <bertrand.uhart@gmail.com>
2018-05-15 15:25:19 +0200

    GA ok

--- src/assets/js/analytics.js
+++ src/assets/js/analytics.js
@@ -8,61 +8,104 @@ console.log("hello");
 }(window,document,'script'));
 
 
+gapi.analytics.ready(function() {
 
-gapi.analytics.ready(function () {
-  doAnalytics()
-});
-
-function doAnalytics(){
-    /**
-   * Authorize the user immediately if the user has already granted access.
-   * If no access has been created, render an authorize button inside the
-   * element with the ID "embed-api-auth-container".
-   */
+//Auth
   gapi.analytics.auth.authorize({
     container: 'embed-api-auth-container',
-    clientid: "",
+    clientid: '759238990855-igjcd55mdvrui12ot1dru4v0saksb3uf.apps.googleusercontent.com'
   });
 
+//VIEWSELECTOR - for multiple account
 
-  /**
-   * Create a new ViewSelector instance to be rendered inside of an
-   * element with the id "view-selector-container".
-   */
-  var viewSelector = new gapi.analytics.ViewSelector({
-    container: 'view-selector-container'
-  });
+var viewSelector1 = new gapi.analytics.ViewSelector({
+  container: 'view-selector-1-container'
+});
 
-  // Render the view selector to the page.
-  viewSelector.execute();
+viewSelector1.execute();
 
 
-  /**
-   * Create a new DataChart instance with the given query parameters
-   * and Google chart options. It will be rendered inside an element
-   * with the id "chart-container".
-   */
-  var dataChart = new gapi.analytics.googleCharts.DataChart({
-    query: {
-      metrics: 'ga:sessions',
-      dimensions: 'ga:date',
-      'start-date': '7daysAgo',
-      'end-date': 'yesterday'
-    },
-    chart: {
-      container: 'chart-container',
-      type: 'LINE',
-      options: {
-        width: '100%'
-      }
+
+
+//DATE CHART
+var dataChart1 = new gapi.analytics.googleCharts.DataChart({
+  query: {
+    metrics: 'ga:sessions',
+    dimensions: 'ga:date',
+    'start-date': '7daysAgo',
+    'end-date': 'yesterday',
+  },
+
+  chart: {
+    container: 'chart-1-container',
+    type: 'LINE',
+    options: {
+      width: '100%'
     }
-  });
+  }
+});
 
 
-  /**
-   * Render the dataChart on the page whenever a new view is selected.
-   */
-  viewSelector.on('change', function(ids) {
-    dataChart.set({query: {ids: ids}}).execute();
-  });
-}
+var dataChart2 = new gapi.analytics.googleCharts.DataChart({
+  query: {
+    metrics: 'ga:sessions',
+    dimensions: 'ga:country',
+    'start-date': '7daysAgo',
+    'end-date': 'yesterday',
+    'max-results': 6,
+    sort: '-ga:sessions'
+  },
+  chart: {
+    container: 'chart-2-container',
+    type: 'PIE',
+    options: {
+      width: '100%',
+      pieHole: 4/9
+    }
+  }
+});
+
+var dataChart3 = new gapi.analytics.googleCharts.DataChart({
+  query: {
+    'dimensions': 'ga:source',
+    'metrics': 'ga:sessions',
+    'start-date': '7daysAgo',
+    'end-date': 'yesterday',
+    'sort': '-ga:sessions',
+    'max-results': '6'
+  },
+  chart: {
+    type: 'TABLE',
+    container: 'chart-3-container',
+    options: {
+      width: '100%'
+    }
+  }
+});
+
+
+
+
+/**
+ * Update the first dataChart when the first view selecter is changed.
+ */
+viewSelector1.on('change', function(ids) {
+  dataChart1.set({query: {ids: ids}}).execute();
+});
+
+/**
+ * Update the second dataChart when the second view selecter is changed.
+ */
+viewSelector1.on('change', function(ids) {
+  dataChart2.set({query: {ids: ids}}).execute();
+});
+
+viewSelector1.on('change', function(ids) {
+  dataChart3.set({query: {ids: ids}}).execute();
+});
+
+
+
+
+
+});
